8ae8e345cb380dedc5b557c062ed75b8c2c1d63f
xjiang300 <xjiang300@gatech.edu>
2017-03-03 15:40:30 -0500

    create API for Manager controller

---
+++ GroupProject/TourneyManager/app/src/main/java/edu/gatech/seclass/tourneymanager/controller/Manager.java
@@ -0,0 +1,30 @@
+package edu.gatech.seclass.tourneymanager.controller;
+
+/**
+ * Created by xiaolu on 3/3/17.
+ * @author Xiaolu Jiang
+ */
+
+public class Manager {
+
+    public Manager() {
+
+    }
+
+
+    public void startTournament() {
+        //TODO
+        // refactor code from StartTournament class
+    }
+
+    public void endTournament() {
+        //TODO
+        // refactor code from MatchList4ManagerMode
+    }
+
+
+    public void endMatch() {
+        //TODO
+        // refactor code from MatchDetailOngoing class
+    }
+}
