e23f468b078c7abce6a809db8d52f75dccc4b40f
Kizhakke Veetil, Vidya <vkv6@gatech.edu>
2017-03-09 15:09:52 -0800

    Added test cases

--- GroupProject/TourneyManager/app/src/androidTest/java/edu/gatech/seclass/tourneymanager/UnitTests.java
+++ GroupProject/TourneyManager/app/src/androidTest/java/edu/gatech/seclass/tourneymanager/UnitTests.java
@@ -19,6 +19,8 @@ import edu.gatech.seclass.tourneymanager.activity.ModeSelector;
 import edu.gatech.seclass.tourneymanager.activity.PlayerList4ManagerMode;
 
 
+import static android.R.*;
+
 import static android.R.attr.button;
 import static edu.gatech.seclass.tourneymanager.R.id.editTextEntryPrice;
 import static edu.gatech.seclass.tourneymanager.R.id.editTextHouseCut;
@@ -50,8 +52,8 @@ public class UnitTests {
     @Test
     public void testAssertions() {
         //test data
-        String name= new String ("abcd");
-        String username = new String ("ab1234");
+        String name = new String("abcd");
+        String username = new String("ab1234");
         String phonenum = "1234567890";
 
         //Check that an object isn't null
@@ -64,112 +66,115 @@ public class UnitTests {
         assertNotNull(phonenum);
 
     }
+
     @Test
 
-        public void testAddPlayerButton(){
+    public void testAddPlayerButton() {
 
         button buttonRegistor;
-        TouchUtils.clickView(this,buttonRegistor);
+        TouchUtils.clickView(this, buttonRegistor);
 
-        }
+    }
 
 
     @Test
-    public void testButtonclear(){
+    public void testButtonclear() {
         button buttonClear;
-        TouchUtils.clickView(this,buttonClear);
+        TouchUtils.clickView(this, buttonClear);
 
-        }
+    }
 
 
-     @Test
-     public void testreturnfromAddPlayerClassbutton() {
-         button buttonReturn;
-         TouchUtils.clickView(PlayerList4ManagerMode.class,buttonReturn);
+    @Test
+    public void testreturnfromAddPlayerClassbutton() {
+        button buttonReturn;
+        TouchUtils.clickView(PlayerList4ManagerMode.class, buttonReturn);
 
-     }
+    }
 
     @Test
     public void testreturnfromManagerModebutton() {
-        button buttonReturn ;
-        TouchUtils.clickView( ModeSelector.class,buttonReturn);
-            }
+        button buttonReturn;
+        TouchUtils.clickView(ModeSelector.class, buttonReturn);
+    }
 
     @Test
     public void testmatchlistbutton() {
         button matchlistbutton;
-        TouchUtils.clickView(MatchList4ManagerMode.class,matchlistbutton);
+        TouchUtils.clickView(MatchList4ManagerMode.class, matchlistbutton);
     }
-     @Test
-     public void testaddplayerfrommanagermodebutton() {
-         button addplayerbutton ;
-         TouchUtils.clickView(PlayerList4ManagerMode.class,addplayerbutton);
-     }
+
+    @Test
+    public void testaddplayerfrommanagermodebutton() {
+        button addplayerbutton;
+        TouchUtils.clickView(PlayerList4ManagerMode.class, addplayerbutton);
+    }
 
 
     @Test
     public void testprofithistorybutton() {
         button profithistorybutton;
-        TouchUtils.clickView(ProfitHistory.class,profithistorybutton);
+        TouchUtils.clickView(ProfitHistory.class, profithistorybutton);
     }
+
     @Test
     public void testmanagetournamentbutton() {
         button managetournamentbutton;
-        TouchUtils.clickView(StartTournament.class,managetournamentbutton);
+        TouchUtils.clickView(StartTournament.class, managetournamentbutton);
     }
 
 
     @Test
     public void testreturnfrom_profithistory_button() {
         button retprofithistory_button;
-        TouchUtils.clickView(ManagerMode.class,retprofithistory_button);
+        TouchUtils.clickView(ManagerMode.class, retprofithistory_button);
 
     }
+
     @Test
     public void testreturnfrom_starttournament_button() {
         button retstarttour_button;
-        TouchUtils.clickView(ManagerMode.class,retstarttour_button);
+        TouchUtils.clickView(ManagerMode.class, retstarttour_button);
 
-    }
-    @ Test
-    public void noduplicateusername(){
-        assertNotSame(editTextUsername,R.id.editTextUsername,0);
     }
 
     @Test
-    public void ongoingmatch(){
+    public void noduplicateusername() {
+        assertNotSame(editTextUsername, R.id.editTextUsername, 0);
+    }
+
+    @Test
+    public void ongoingmatch() {
         MatchRepo matchRepo = new MatchRepo(this);
         Match match = new Match();
         match = matchRepo.getMatchById(match_id);
         String status = match.getStatus();
 
-      assertSame("match ongoing",status,Match.STATUS_ONGOING);
+        assertSame("match ongoing", status, Match.STATUS_ONGOING);
 
     }
 
     @Test
-    public void endmatch()
-    {
+    public void endmatch() {
         MatchRepo matchRepo = new MatchRepo(this);
         Match match = new Match();
         match = matchRepo.getMatchById(match_id);
         String status = match.getStatus();
-        assertSame("end of match",status,Match.STATUS_FINISHED);
+        assertSame("end of match", status, Match.STATUS_FINISHED);
     }
 
     @Test
-    public void addmatch()
-    {
+    public void addmatch() {
         MatchRepo matchRepo = new MatchRepo(this);
         Match match = new Match();
         match = matchRepo.getMatchById(match_id);
         String status = match.getStatus();
-        assertSame("match ready",status,Match.STATUS_READY);
+        assertSame("match ready", status, Match.STATUS_READY);
         matchRepo.update(match);
     }
 
     @Test
-    public void calculate&display_houseprofit() {
+    public void calculate_display_houseprofit() {
         int houseCut = 0;
         int houseProfit = 0;
         int entry = 0;
@@ -178,7 +183,8 @@ public class UnitTests {
         houseCut = (Integer) (editTextHouseCut);
         entry = (Integer) (editTextEntryPrice);
         houseProfit = entry * num_player * houseCut / 100;
-        Toast.makeText("house profit calculated",houseProfit,5);
+        Toast.makeText("house profit calculated", houseProfit, 5);
         Assert.assertTrue("the house profit is displayed", 1);
     }
+}
 
