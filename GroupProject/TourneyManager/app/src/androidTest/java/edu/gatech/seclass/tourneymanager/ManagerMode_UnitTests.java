2c702ca0db6bf801b8dbd78ffb6a09e2519756d0
Kizhakke Veetil, Vidya <vkv6@gatech.edu>
2017-03-09 17:05:34 -0800

    added unit test cases

--- GroupProject/TourneyManager/app/src/androidTest/java/edu/gatech/seclass/tourneymanager/ManagerMode_UnitTests.java
+++ GroupProject/TourneyManager/app/src/androidTest/java/edu/gatech/seclass/tourneymanager/ManagerMode_UnitTests.java
@@ -26,7 +26,7 @@ public class ManagerMode_UnitTests {
 
       @Test
         public void testAddPlayerbutton() {
-          assert managerMode.buttonAddPlayer());
+          assert managerMode.buttonAddPlayer();
       }
 
          @Test
@@ -51,56 +51,7 @@ public class ManagerMode_UnitTests {
              assert(managerMode.buttonManageTournament());
          }
 
-        @Test
-        public void ongoingmatch() {
-            MatchRepo matchRepo = new MatchRepo(this);
-            Match match = new Match();
-            match = matchRepo.getMatchById(match_id);
-            String status = match.getStatus();
 
-            assertSame("match ongoing", status, Match.STATUS_ONGOING);
-
-        }
-
-        @Test
-        public void endmatch() {
-            MatchRepo matchRepo = new MatchRepo(this);
-            Match match = new Match();
-            match = matchRepo.getMatchById(match_id);
-            String status = match.getStatus();
-            assertSame("end of match", status, Match.STATUS_FINISHED);
-        }
-
-        @Test
-        public void addmatch() {
-            MatchRepo matchRepo = new MatchRepo(this);
-            Match match = new Match();
-            match = matchRepo.getMatchById(match_id);
-            String status = match.getStatus();
-            assertSame("match ready", status, Match.STATUS_READY);
-            matchRepo.update(match);
-        }
-
-
-
-        @Test
-        public void test_matchlist()
-        MatchRepo matchRepo = new MatchRepo(this);
-        Match match = new Match();
-        List<Map<String, String>> matchList = matchRepo.getMatchList();
-        ListAdapter adapter = new SimpleAdapter(this,
-                matchList,
-                R.layout.view_match_entry,
-                new String[] {"id", "player1name", "player2name", "round", "winnerName", "status"},
-                new int[] {R.id.match_id, R.id.player1_name, R.id.player2_name, R.id.match_round,
-                        R.id.match_winner, R.id.match_status});
-
-
-    }
-
-    String status = match.getStatus();
-
-}
 
 
 }
