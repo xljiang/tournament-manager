22ec920d13739b45cafd8c2f55d95447c69aa084
Kizhakke Veetil, Vidya <vkv6@gatech.edu>
2017-03-10 12:38:48 -0800

    updated test case

--- GroupProject/TourneyManager/app/src/test/java/edu/gatech/seclass/tourneymanager/ModeSelector_UnitTests.java
+++ GroupProject/TourneyManager/app/src/test/java/edu/gatech/seclass/tourneymanager/ModeSelector_UnitTests.java
@@ -17,6 +17,20 @@ import edu.gatech.seclass.tourneymanager.activity.ModeSelector;
 
 public class ModeSelector_UnitTests extends ModeSelector {
     private ModeSelector modeSelector;
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_mode_selector);
+        Button b = (Button) findViewById(R.id.buttonManagerMode);
+        Button b1= (Button)findViewById(R.id.buttonPlayerMode);
+        Button b2= (Button)findViewById(R.id.btnBack);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent i = new Intent(ModeSelector_UnitTests.this,ModeSelector.class);
+                startActivity(i);
+            }
+        });
+
+    }
 
     @Before
     public void setUp() {
@@ -28,21 +42,46 @@ public class ModeSelector_UnitTests extends ModeSelector {
         modeSelector = null;
     }
 
-    @Test
 
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.activity_mode_selector);
-        Button b = (Button) findViewById(R.id.buttonManagerMode);
-        Button b1= (Button)findViewById(R.id.buttonPlayerMode);
-        Button b2= (Button)findViewById(R.id.btnBack);
-        b.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                Intent i = new Intent(ModeSelector_UnitTests.this, ModeSelector.class);
-                startActivity(i);
+
+
+
+    @Test
+    public void testbuttonManagerMode() throws Throwable {
+        runOnUiThread(new Runnable() {
+            Button bmanagermode = (Button) findViewById(R.id.buttonManagerMode);
+
+            @Override
+            public void run() {
+                org.junit.Assert.assertTrue(bmanagermode.performClick());
+
             }
         });
+    }
 
+    @Test
+    public void testbuttonPlayerMode() throws Throwable {
+        runOnUiThread(new Runnable() {
+            Button bplayermode= (Button) findViewById(R.id.buttonPlayerMode);
+
+            @Override
+            public void run() {
+                org.junit.Assert.assertTrue(bplayermode.performClick());
+
+            }
+        });
+    }
+    @Test
+    public void testbuttonback() throws Throwable {
+        runOnUiThread(new Runnable() {
+            Button buttonback = (Button) findViewById(R.id.btnBack);
+
+            @Override
+            public void run() {
+                org.junit.Assert.assertTrue(buttonback.performClick());
+
+            }
+        });
     }
 
 }
